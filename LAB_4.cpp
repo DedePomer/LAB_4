a00cd59e76d3f6a124347984466ec6176064b052
Данила Чирков <t343st@gmail.com>
2023-10-22 19:07:11 +0300

    -

--- LAB_4.cpp
+++ LAB_4.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int ind = 0;
+
 
 int countNum(int n)
 {
@@ -23,15 +23,12 @@ int countNum(int n)
 }
 int minNum(int n)
 {
-	ind = 1;
-	int minN = n % 10, m, i=1;
+	int minN = n % 10, m;
 	while (n > 0)
 	{
-		i++;
 		m = n % 10;
 		if (minN > m)
 		{
-			ind = i;
 			minN = m;
 		}
 		n = n / 10;
@@ -39,48 +36,32 @@ int minNum(int n)
 	}
 	return minN;
 }
-int POW10(int n, int m)
-{
-	int c = 1;
-	for (int i = 1; i <= m; i++)
-	{
-		c = c * n;
-	}
-	return c;
-}
 
 void main()
 {
 	setlocale(LC_ALL, "RUS");
-	int n, c, nw=0,N;
-	bool tf = true;
-	/*cout << "Введите число ";
-	cin >> n;*/
-	n = 523;
+	int n, c, nw=0,N,MIN;
+	cout << "Enter a number ";
+	cin >> n;
 	c = countNum(n);
 	
-
 	for (int i = 0; i < c; i++)
 	{
-		nw = nw * 10 + minNum(n);
+		MIN = minNum(n);
+		nw = nw * 10 + MIN;
 		N = 0;
-		for (int j =1; j <= countNum(n); j++)
-		{		
-			if (j == ind)
+
+		while (n > 0)
+		{ 
+			if (n % 10 != MIN)
 			{
-				tf = false;
+				N = N * 10 + n % 10;
 			}
-			else
-			{
-				if (tf)
-					N = N * 10 + n % POW10(10, j);// проблемс
-				else
-					N = N * 10 + n % POW10(10, j - 1);
-			}
+			n = n / 10;
 		}
 		n = N;
-		tf = true;
 	}
+	cout << nw;
 }
 
 
